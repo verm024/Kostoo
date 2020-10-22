b0cbd7c9cc23609d79d8b9f93108baffb4c72c0e
yusufyp <51522321+yusufyp@users.noreply.github.com>
2020-10-22 20:48:28 +0700

    Tampilan Dashboard Investor

---
+++ kostoo-webapp/src/components/TopNav.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="top-nav">
+    <svg
+      class="humburger-menu"
+      width="30"
+      height="26"
+      viewBox="0 0 24 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 2H22"
+        stroke="#5BC77A"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+      <path
+        d="M2 10H15"
+        stroke="#5BC77A"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+      <path
+        d="M2 18H22"
+        stroke="#5BC77A"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+    </svg>
+
+    <h3 class="nama-halaman">
+      Proyek
+    </h3>
+
+    <svg
+      class="bell-icon"
+      width="24"
+      height="30"
+      viewBox="0 0 20 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 26C11.375 26 12.5 24.8 12.5 23.3333H7.5C7.5 24.8 8.6125 26 10 26ZM17.5 18V11.3333C17.5 7.24 15.45 3.81333 11.875 2.90667V2C11.875 0.893333 11.0375 0 10 0C8.9625 0 8.125 0.893333 8.125 2V2.90667C4.5375 3.81333 2.5 7.22667 2.5 11.3333V18L0 20.6667V22H20V20.6667L17.5 18Z"
+        fill="#5BC77A"
+      />
+    </svg>
+  </div>
+</template>
+<style scoped>
+@import url("../assets/css/navbar.css");
+</style>
--- kostoo-webapp/src/views/Home.vue
+++ kostoo-webapp/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <topNav />
+    <div class="view">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import topNav from "@/components/TopNav";
 
 export default {
-  name: "Home",
   components: {
-    HelloWorld
+    topNav
   }
 };
 </script>
